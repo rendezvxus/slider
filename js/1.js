3dd369b449b4707060ee21bc662cd85359bf4d3b
rendezvxus <realsuppaplex@gmail.com>
2026-02-19 14:48:12 +0300

    Arrows n pagination

--- js/1.js
+++ js/1.js
@@ -1,1 +1,11 @@
-new Swiper('.image-slider');
+new Swiper('.image-slider',{
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+    }
+});
